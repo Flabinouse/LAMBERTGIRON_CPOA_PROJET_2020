b35a62c652afd465eb1f5832f06467affaec99df
Max <Max@172.19.170.145>
2020-10-30 18:10:02 +0100

    Modif AjoutClientController

--- LAMBERTGIRON_CPOA_PROJET_2020/src/controller/AjoutClientController.java
+++ LAMBERTGIRON_CPOA_PROJET_2020/src/controller/AjoutClientController.java
@@ -2,18 +2,49 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import modele.dao.factory.DAOFactory;
-import modele.dao.factory.Persistance;
 import modele.metier.Client;
 
-public class AjoutClientController {
+public class AjoutClientController implements IAjoutModif<Client> {
 
-	private DAOFactory dao = DAOFactory.getDAOFactory(Persistance.MYSQL);
+	private DAOFactory dao;
+
+	private MainController main;
+
+	private enumAction action;
+
+	private Client cli;
+
+	public void defineTF(String nom, String prenom, String identifiant, String mdp, String numero, 
+			String rue, String postal, String ville, String pays) {
+
+		idTextNom.setText(nom);
+
+		idTextPrenom.setText(prenom);
+		
+		idTextIdent.setText(identifiant);
+
+		idTextMdp.setText(mdp);
+		
+		idTextNum.setText(numero);
+
+		idTextRue.setText(rue);
+		
+		idTextPostal.setText(postal);
+
+		idTextVille.setText(ville);
+		
+		idTextPays.setText(pays);		
+	}
 
 	@FXML
 	private TextField idTextNom;
@@ -47,66 +78,146 @@ public class AjoutClientController {
 
 	@FXML
 	private Label idLabelAffi;
-
-	@FXML
-	public void creerClient(ActionEvent event) throws Exception {
-
+	
+	@Override
+	public void create(FXMLLoader fxmlLoader, DAOFactory daoF, Stage stage, MainController mainC, enumAction actionC) {
 		try {
-			String nom = this.idTextNom.getText().trim();
-			String prenom = this.idTextPrenom.getText().trim();
-			String ident = this.idTextIdent.getText().trim();
-			String mdp = this.idTextMdp.getText().trim();
-			String numero = this.idTextNum.getText().trim();
-			String rue = this.idTextRue.getText().trim();
-			String postal = this.idTextPostal.getText().trim();
-			String ville = this.idTextVille.getText().trim();
-			String pays = this.idTextPays.getText().trim();
-			Client cli = new Client();
-			cli.setNom(nom);
-			cli.setPrenom(prenom);
-			cli.setIdentifiant(ident);
-			cli.setMdp(mdp);
-			cli.setNumero(numero);
-			cli.setRue(rue);
-			cli.setPostal(postal);
-			cli.setVille(ville);
-			cli.setPays(pays);
-			dao.getClientDAO().create(cli);
-			Alert alert = new Alert(Alert.AlertType.INFORMATION);
-			alert.setTitle("Ajout d'un client");
-			alert.setHeaderText("Client cree : " + nom + " " + prenom + " " + ident + " " + mdp + " " + numero + " "
-					+ rue + " " + postal + " " + ville + " " + pays);
-			alert.showAndWait();
-			Stage stage = (Stage) idBoutonValider.getScene().getWindow();
-			stage.close();
 
-		} catch (IllegalArgumentException e) {
-			this.idLabelAffi.setStyle("-fx-text-fill: red;");
-			this.idLabelAffi.setText("Veuillez saisir tous les champs");
+			Parent root = (Parent) fxmlLoader.load();
+
+			AjoutClientController param = fxmlLoader.getController();
+			param.action = actionC;
+			param.dao = daoF;
+			param.main = mainC;
+
+			stage = new Stage();
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setTitle("Ajout d'un Client");
+			stage.setScene(new Scene(root));
+			stage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Override
+	public void update(FXMLLoader fxmlLoader, DAOFactory daoF, Stage stage, MainController mainC, enumAction actionU,
+			Client cliU) {
+		try {
+
+			Parent root = (Parent) fxmlLoader.load();
+
+			AjoutClientController param = fxmlLoader.getController();
+			param.action = actionU;
+			param.dao = daoF;
+			param.main = mainC;
+			param.cli = cliU;
+
+			String nom = cliU.getNom();
+			String prenom = cliU.getPrenom();
+			String identifiant = cliU.getIdentifiant();
+			String mdp = cliU.getMdp();
+			String numero = cliU.getNumero();
+			String rue = cliU.getRue();
+			String postal = cliU.getPostal();
+			String ville = cliU.getVille();
+			String pays = cliU.getPays();
+
+			param.defineTF(nom, prenom, identifiant, mdp, numero, rue, postal, ville, pays);
+
+			stage = new Stage();
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setTitle("Modification d'un Client");
+			stage.setScene(new Scene(root));
+			stage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-
+	
 	@FXML
-	public void modifClient(ActionEvent event) throws Exception {
-		/*
-		 * try { String nom = this.idTextNom.getText().trim(); String description =
-		 * this.idTextDesc.getText().trim(); float tarif =
-		 * Float.parseFloat(this.idTextTarif.getText().trim()); Produit prod = new
-		 * Produit(); prod.setId_produit(0);//a modif prod.setNom(nom);
-		 * prod.setTarif(tarif); prod.setDescription(description);
-		 * prod.setVisuel(this.idChoixCateg.getValue().getVisuel());
-		 * prod.setId_categorie(this.idChoixCateg.getValue().getId_categorie());
-		 * dao.getProduitDAO().update(prod); Alert alert=new
-		 * Alert(Alert.AlertType.INFORMATION); alert.setTitle("Ajout d'un produit");
-		 * alert.setHeaderText("Produit créer : " + nom + " " + description + "(" +
-		 * this.idChoixCateg.getValue().getTitre() + "), " + tarif);
-		 * alert.showAndWait(); Stage stage = (Stage)
-		 * idBoutonCreer.getScene().getWindow(); stage.close();
-		 * 
-		 * } catch (IllegalArgumentException e) {
-		 * this.idLabelRes.setStyle("-fx-text-fill: red;");
-		 * this.idLabelRes.setText("Veuillez saisir tous les champs"); }
-		 */
+	public void validation(ActionEvent event) throws Exception {
+		if (action == enumAction.Create) {
+			try {
+				String nom = this.idTextNom.getText().trim();
+				String prenom = this.idTextPrenom.getText().trim();
+				String ident = this.idTextIdent.getText().trim();
+				String mdp = this.idTextMdp.getText().trim();
+				String numero = this.idTextNum.getText().trim();
+				String rue = this.idTextRue.getText().trim();
+				String postal = this.idTextPostal.getText().trim();
+				String ville = this.idTextVille.getText().trim();
+				String pays = this.idTextPays.getText().trim();
+				Client cliA = new Client();
+				cliA.setNom(this.idTextNom.getText().trim());
+				cliA.setPrenom(this.idTextPrenom.getText().trim());
+				cliA.setIdentifiant(this.idTextIdent.getText().trim());
+				cliA.setMdp(this.idTextMdp.getText().trim());
+				cliA.setNumero(this.idTextNum.getText().trim());
+				cliA.setRue(this.idTextRue.getText().trim());
+				cliA.setPostal(this.idTextPostal.getText().trim());
+				cliA.setVille(this.idTextVille.getText().trim());
+				cliA.setPays(this.idTextPays.getText().trim());
+
+				dao.getClientDAO().create(cliA);
+
+				Alert alert = new Alert(Alert.AlertType.INFORMATION);
+				alert.setTitle("Ajout d'un Client");
+				alert.setHeaderText("Client cree : " + nom + " " + prenom + " " + ident + " " + mdp + " " + numero + " "
+						+ rue + " " + postal + " " + ville + " " + pays);
+				alert.showAndWait();
+
+				main.tableUpdate();
+				Stage stage = (Stage) idBoutonValider.getScene().getWindow();
+				stage.close();
+
+			} catch (IllegalArgumentException e) {
+				this.idLabelAffi.setStyle("-fx-text-fill: red;");
+				this.idLabelAffi.setText("Veuillez saisir tous les champs");
+			}
+		} else {
+			try {
+				String nom = this.idTextNom.getText().trim();
+				String prenom = this.idTextPrenom.getText().trim();
+				String ident = this.idTextIdent.getText().trim();
+				String mdp = this.idTextMdp.getText().trim();
+				String numero = this.idTextNum.getText().trim();
+				String rue = this.idTextRue.getText().trim();
+				String postal = this.idTextPostal.getText().trim();
+				String ville = this.idTextVille.getText().trim();
+				String pays = this.idTextPays.getText().trim();
+				cli.setNom(this.idTextNom.getText().trim());
+				cli.setPrenom(this.idTextPrenom.getText().trim());
+				cli.setIdentifiant(this.idTextIdent.getText().trim());
+				cli.setMdp(this.idTextMdp.getText().trim());
+				cli.setNumero(this.idTextNum.getText().trim());
+				cli.setRue(this.idTextRue.getText().trim());
+				cli.setPostal(this.idTextPostal.getText().trim());
+				cli.setVille(this.idTextVille.getText().trim());
+				cli.setPays(this.idTextPays.getText().trim());
+
+				dao.getClientDAO().update(cli);
+
+				Alert alert = new Alert(Alert.AlertType.INFORMATION);
+				alert.setTitle("Ajout d'une categorie");
+				alert.setHeaderText("Client modifie : " + nom + " " + prenom + " " + ident + " " + mdp + " " + numero + " "
+						+ rue + " " + postal + " " + ville + " " + pays);
+				alert.showAndWait();
+
+				main.tableUpdate();
+				Stage stage = (Stage) idBoutonValider.getScene().getWindow();
+				stage.close();
+
+			} catch (IllegalArgumentException e) {
+				this.idLabelAffi.setStyle("-fx-text-fill: red;");
+				this.idLabelAffi.setText("Veuillez saisir tous les champs");
+			}
+
+		}
+
 	}
 
 }
